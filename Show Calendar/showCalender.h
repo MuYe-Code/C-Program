4bb6446bd3f62fb74d499bc5a782e40007a5ef7d
MuYe-Code <77724391+MuYe-Code@users.noreply.github.com>
2022-05-12 12:53:36 +0800

    Create showCalender.h

---
+++ Show Calendar/showCalender.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int isLeapYear(int year);
+int isLongerMonth(int month);
+int nextDay(int year, int month, int day);
+int numOfDaysBetweenTwoDays(int year1, int month1, int day1, int year2, int month2, int day2);
+int whatDay(int year, int month, int day);
+void showCalendar(int year);
